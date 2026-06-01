80807a4954e450b83e2a2374ef3f10ba5186df9c
Long <longs3515@gmail.com>
2026-06-01 14:02:50 +0700

    cs01

--- cs01/Program.cs
+++ cs01/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+namespace CS01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            List<int> numbers = new List<int> { 5, 12, 6, 7, 9, 11 };
+            Func<int, bool> isEven = n =>n %2 ==0;
+            foreach(var number in numbers){
+                bool res=isEven(number);
+                if (res)
+                {
+                    Console.WriteLine(number);
+                }
+            }     
+        }
+    }
+}
