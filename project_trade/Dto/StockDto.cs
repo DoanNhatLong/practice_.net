1d7a93f8ea450cec1514d1b05e3c94317afd6c39
Long <longs3515@gmail.com>
2026-06-12 11:50:42 +0700

    test Dto

---
+++ project_trade/Dto/StockDto.cs
@@ -0,0 +1,1 @@
+namespace project_trade.Dto;
--- project_trade/obj/Debug/net8.0/project_trade.AssemblyInfo.cs
+++ project_trade/obj/Debug/net8.0/project_trade.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("project_trade")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a039928dcf5b3955bb821a52cb7aeaa0346410a1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c286c9800743727c69f47422c71c939b4038099f")]
 [assembly: System.Reflection.AssemblyProductAttribute("project_trade")]
 [assembly: System.Reflection.AssemblyTitleAttribute("project_trade")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
